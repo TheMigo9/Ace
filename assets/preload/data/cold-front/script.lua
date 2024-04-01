caa8f005ef8e269adbc17d5f9f07d1d892adf834
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-04-01 23:22:19 +0200

    Add files via upload

---
+++ assets/preload/data/cold-front/script.lua
@@ -0,0 +1,32 @@
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true)
+
+		if getProperty("bfVersion") == "bf-retro" then
+			startDialogue('dialogue-retro', 'dialogueAmbience2')
+		elseif getProperty("bfVersion") == "bf-ace" then
+			startDialogue('dialogue-ace', 'dialogueAmbience2')
+		else
+			startDialogue('dialogue', 'dialogueAmbience2')
+		end
+		allowCountdown = true
+		return Function_Stop
+	end
+	runTimer("closeScript", 0.1)
+	return Function_Continue
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'closeScript' then -- Timer completed, play dialogue
+		close(true)
+	end
+end
+
+function onNextDialogue(count)
+
+end
+
+function onSkipDialogue(count)
+
+end

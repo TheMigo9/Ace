a1c60ba105a49ed1c2d828856f2e88cd137c5b12
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-04-01 23:32:30 +0200

    Add files via upload

---
+++ assets/preload/data/ectospasm/script.lua
@@ -0,0 +1,27 @@
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and not isStoryMode and not seenCutscene then
+		--setProperty('inCutscene', true)
+
+		startDialogue('dialogue', 'dialogueAmbience1')
+
+		allowCountdown = true
+		return Function_Stop
+	end
+	runTimer("closeScript", 0.1)
+	return Function_Continue
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'closeScript' then -- Timer completed, play dialogue
+		close(true)
+	end
+end
+
+function onNextDialogue(count)
+
+end
+
+function onSkipDialogue(count)
+
+end

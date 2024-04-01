59246305f161679c142ae3d24da3a3e2a3fa5302
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-04-01 23:42:01 +0200

    Add files via upload

---
+++ assets/preload/data/no-homo/script.lua
@@ -0,0 +1,36 @@
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true)
+		startDialogue('dialogue', 'minusAmbience')
+		allowCountdown = true
+		return Function_Stop
+	end
+	return Function_Continue
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then -- Timer completed, play dialogue
+
+	end
+end
+
+function onNextDialogue(count)
+
+end
+
+function onSkipDialogue(count)
+
+end
+
+local allowEndShit = false
+
+function onEndSong()
+	if not allowEndShit and isStoryMode then
+ 		setProperty('inCutscene', true);
+ 		startDialogue('dialogue2', 'minusAmbience');
+  		allowEndShit = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
